546f85d442231a1fbea61223c04c98e20811db17
michelspirlandeli <michelspirlandeli10@gmail.com>
2018-10-13 10:23:32 -0300

    Managing Server Errors

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ const route = router.get('/', (req, res, next) => {
 app.use('/', route);
 
 server.listen(port)
+server.on('error', onError);
 console.log('API rodando na porta ' + port);
 
 function normalizePort(val){
@@ -34,4 +35,27 @@ function normalizePort(val){
     }
     
     return false;
+}
+
+function onError(error){
+    if(error.syscall !== 'listen'){
+        throw error;
+    }
+
+    const bind = typeof port === 'string' ?
+        'Pipe' + port: 
+        'Port' + port;
+    
+    switch (error.code){
+        case 'EACCES': 
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE': 
+            console.error(bind + 'is already in use');
+            process.exit(1);
+            break;
+        default: 
+            throw error;
+    }
 }
