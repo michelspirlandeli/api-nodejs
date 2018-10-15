c33521d465c6d0a866e811f6d7040050bb930f46
michelspirlandeli <michelspirlandeli10@gmail.com>
2018-10-15 14:06:45 -0300

    separating the server

--- bin/server.js
+++ bin/server.js
@@ -1,23 +1,11 @@
-'use strict'
+const app    = require('../src/app');
+const debug = require('debug')('nodestr:server');
+const http  = require('http');
 
-const http    = require('http');
-const debug   = require('debug')('nodestr:server');
-const express = require('express');
-
-const app  = express();
 const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
 const server = http.createServer(app);
-const router = express.Router();
-
-const route = router.get('/', (req, res, next) => {
-    res.status(200).send({
-        title  : "Node Store API",
-        version: "0.0.1"
-    });
-});
-app.use('/', route);
 
 server.listen(port)
 server.on('error', onError);
@@ -65,6 +53,6 @@ function onListening(){
     const addr = server.address();
     const bind = typeof addr === 'string'
         ? 'pipe' + addr
-        :                   'port' + addr.port;
+        :                             'port' + addr.port;
     debug('Listening on ' + bind);
 }
