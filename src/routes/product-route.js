da02be48f2d4518a14e18acef04b3636b3c63998
michelspirlandeli <michelspirlandeli10@gmail.com>
2018-10-23 10:20:37 -0300

    create controllers

--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -1,22 +1,11 @@
 'use strict';
 
-const express = require('express');
-const router  = express.Router();
+const express    = require('express');
+const router     = express.Router();
+const controller = require('../controllers/product-controller');
 
-router.post('/', (req, res, next) => {
-    res.status(201).send(req.body);
-});
-
-router.put('/:id', (req, res, next) => {
-    const id = req.params.id;
-    res.status(200).send({
-        id  : id,
-        item: req.body
-    });
-});
-
-router.delete('/', (req, res, next) => {
-    res.status(201).send(req.body);
-});
+router.post('/', controller.post);
+router.put('/:id', controller.put);
+router.delete('/', controller.delete);
 
 module.exports = router;
