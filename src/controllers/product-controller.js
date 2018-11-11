f37e24dc800b9a4eaebe4b8275a65595ecd45139
michelspirlandeli <michelspirlandeli10@gmail.com>
2018-11-11 09:34:00 -0200

    delete a product

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -90,6 +90,17 @@ exports.put =  (req, res, next) => {
 };
 
 exports.delete =  (req, res, next) => {
-    res.status(201).send(req.body);
+    Product
+    .findOneAndRemove(req.params.id)
+    .then(x => {
+      res.status(200).send({
+         message:'Produto removido com sucesso!'
+      });
+    }).catch(e =>{
+        res.status(400).send({
+          message: 'Falha ao remover o produto',
+          data: e
+      });
+    });
 };
 
--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -10,6 +10,6 @@ router.get('/admin/:id', controller.getById);
 router.get('/tags/:tag', controller.getByTag);
 router.post('/', controller.post);
 router.put('/:id', controller.put);
-router.delete('/', controller.delete);
+router.delete('/:id', controller.delete);
 
 module.exports = router;
